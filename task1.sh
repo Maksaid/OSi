880e47f763f03d48bfcdb7fb5ff6b284e847533e
Maksaid <58601553+Maksaid@users.noreply.github.com>
2022-10-01 12:29:32 +0300

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $1 -ge $2 ]] && [[ $1 -ge $3 ]]
-   then echo "Максимум: $1"
+   then echo "$1"
 elif [[ $2 -ge $1 ]] && [[ $2 -ge $3 ]]
-   then echo "Максимум: $2"
+   then echo "$2"
 else
-   echo "Максимум: $3"
+   echo "$3"
 fi
