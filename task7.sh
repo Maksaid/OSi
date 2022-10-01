a17b3170ec2f000d9a68cbdbea8f44aa340fbeda
Maksaid <58601553+Maksaid@users.noreply.github.com>
2022-10-01 13:06:56 +0300

    Update task7.sh

--- task7.sh
+++ task7.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# -E interpret PATTERN as an  extended regular exression  -r recurion  -o only matching
+# \b - ampty string at the edge of the word \ - ecraniruet meta symbols
 pattern="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2, 6}\n"
 echo grep -E -r -o $pattern /etc | tr "\n" "," > emails.lst
