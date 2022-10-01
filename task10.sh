8f71151e97f0c29e296bb2f1b32d53137bfef23f
Maksaid <58601553+Maksaid@users.noreply.github.com>
2022-10-01 13:44:09 +0300

    Update task10.sh

--- task10.sh
+++ task10.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-man bash | grep -o -i "[A-Za-z]\{4,\}" | tr "[A-Z]" "[a-z]" | sort | uniq -c | sort -nr | head -3
+# -o inly matching -i ignore case -c count uniq  sort -nr numereic + reverse -3 print first three
+man bash | grep -o -i "[A-Za-z]\{4,\}" | sort | uniq -c | sort -nr | head -3
