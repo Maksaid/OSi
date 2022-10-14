256f71e7ab705eceb4e39075def189c89db05e31
Maksaid <58601553+Maksaid@users.noreply.github.com>
2022-10-14 13:53:28 +0300

    Delete task10.sh

--- task10.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# -o inly matching -i ignore case -c count uniq  sort -nr numereic + reverse -3 print first three
-man bash | grep -o -i "[A-Za-z]\{4,\}" | sort | uniq -c | sort -nr | head -3
